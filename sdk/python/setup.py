000942d1c515be557805172d1fbe8f7f3ba2fc7b
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-06-30 15:59:15 +0100

    Create setup.py

---
+++ sdk/python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atrop-sdk',
+    version='0.1.0',
+    description='ATROP Python SDK',
+    author='Mahmoud Tawfeek',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+)
