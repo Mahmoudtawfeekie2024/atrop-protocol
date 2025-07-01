22456626c105b91f147e35d27d31e744bff62c50
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-01 10:19:39 +0100

    Update __init__.py

--- sdk/python/src/atrop_sdk/__init__.py
+++ sdk/python/src/atrop_sdk/__init__.py
@@ -1,6 +1,3 @@
-import pytest  # noqa: F401
-from atrop_sdk import core
+from . import core
 
-# Dummy test to satisfy CI until real tests are added
-def test_dummy():
-    assert True
+__all__ = ["core"]
