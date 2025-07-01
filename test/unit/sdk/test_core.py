fc78d922b84beb26e0e63c292e7be8bb9be6e2ab
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-01 10:53:58 +0100

    Update test_core.py

--- test/unit/sdk/test_core.py
+++ test/unit/sdk/test_core.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../sdk/python/src')))
+
 import pytest  # noqa: F401
 from atrop_sdk import core  # noqa: F401
 
