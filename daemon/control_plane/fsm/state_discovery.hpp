acf7fe6716ddafe62f4abd4370e0641a8dcd32ad
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-03 10:39:02 +0100

    Create state_discovery.hpp

---
+++ daemon/control_plane/fsm/state_discovery.hpp
@@ -0,0 +1,22 @@
+// daemon/control_plane/fsm/state_discovery.hpp
+#pragma once
+
+#include "state_base.hpp"
+#include "logger.hpp"
+#include "sdk/c++/config_loader.hpp"
+
+namespace atrop::fsm {
+
+class DiscoveryState : public State {
+public:
+    DiscoveryState();
+    FSMStateID id() const override;
+    void enter() override;
+    void exit() override;
+    FSMStateID handle_event(FSMEvent event) override;
+
+private:
+    std::string discovery_method_;
+};
+
+} // namespace atrop::fsm
