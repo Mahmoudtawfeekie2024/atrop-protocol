38548ff8e1231c8b46f5c85b5a11b4fb20e9e83f
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-03 07:05:34 +0100

    Create discovery_handler.cpp

---
+++ daemon/control_plane/handlers/discovery_handler.cpp
@@ -0,0 +1,19 @@
+#include "discovery_handler.hpp"
+#include "logger.hpp"
+#include <iostream>
+
+namespace atrop {
+namespace control_plane {
+
+void handleDiscoveryPacket(const std::vector<uint8_t>& packet) {
+    LOG_INFO("🧭 Received Discovery Packet | Size: {} bytes", packet.size());
+
+    // TODO: Parse Node Identity Vector (NIV)
+    // TODO: Parse Path Intelligence Vector (PIV)
+    // TODO: Parse Intent Descriptor (IDR)
+
+    // FIXME: Integrate with FSM dispatch
+}
+
+}  // namespace control_plane
+}  // namespace atrop
