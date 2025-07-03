75eb028143d170bba0a95b9dee08b9cbb51cef36
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-03 10:42:52 +0100

    Create state_learn.hpp

---
+++ daemon/control_plane/fsm/state_learn.hpp
@@ -0,0 +1,23 @@
+// daemon/control_plane/fsm/state_learn.hpp
+#pragma once
+
+#include "state_base.hpp"
+#include "logger.hpp"
+#include "sdk/c++/config_loader.hpp"
+
+namespace atrop::fsm {
+
+class LearnState : public State {
+public:
+    LearnState();
+    FSMStateID id() const override;
+    void enter() override;
+    void exit() override;
+    FSMStateID handle_event(FSMEvent event) override;
+
+private:
+    std::string model_type_;
+    bool pretrained_;
+};
+
+} // namespace atrop::fsm
