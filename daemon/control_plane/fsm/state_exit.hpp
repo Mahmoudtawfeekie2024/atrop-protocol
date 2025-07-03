e2cc4fae3f90938f10ee2a675a757d4155ec717b
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-03 18:31:45 +0100

    Update state_exit.hpp

--- daemon/control_plane/fsm/state_exit.hpp
+++ daemon/control_plane/fsm/state_exit.hpp
@@ -1,22 +1,14 @@
-// daemon/control_plane/fsm/state_exit.hpp
 #pragma once
-
 #include "state_base.hpp"
-#include "../common/logger.hpp"
-#include "sdk/c++/config_loader.hpp"
 
 namespace atrop::fsm {
 
 class ExitState : public State {
 public:
-    ExitState();
     FSMStateID id() const override;
     void enter() override;
     void exit() override;
     FSMStateID handle_event(FSMEvent event) override;
-
-private:
-    bool secure_mode_;
 };
 
 } // namespace atrop::fsm
