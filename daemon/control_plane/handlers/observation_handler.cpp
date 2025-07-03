03ecc4008ccf0f467fd553ad2a92c7085cb898c3
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-03 07:06:33 +0100

    Create observation_handler.cpp

---
+++ daemon/control_plane/handlers/observation_handler.cpp
@@ -0,0 +1,20 @@
+#include "observation_handler.hpp"
+#include "logger.hpp"
+#include <iostream>
+
+namespace atrop {
+namespace control_plane {
+
+void handleObservationPacket(const std::vector<uint8_t>& packet) {
+    LOG_INFO("👁️ Received Observation Packet | Size: {} bytes", packet.size());
+
+    // TODO: Parse Node Identity Vector (NIV)
+    // TODO: Parse Path Intelligence Vector (PIV)
+    // TODO: Parse Intent Descriptor (IDR)
+    // TODO: Parse Feedback Injection Field (FIF)
+
+    // FIXME: Integrate with FSM dispatch
+}
+
+}  // namespace control_plane
+}  // namespace atrop
