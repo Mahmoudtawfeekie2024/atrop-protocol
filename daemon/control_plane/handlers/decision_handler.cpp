def2fb77229b9f50c12fd98550a52968a1271d96
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-03 07:06:03 +0100

    Create decision_handler.cpp

---
+++ daemon/control_plane/handlers/decision_handler.cpp
@@ -0,0 +1,19 @@
+#include "decision_handler.hpp"
+#include "logger.hpp"
+#include <iostream>
+
+namespace atrop {
+namespace control_plane {
+
+void handleDecisionPacket(const std::vector<uint8_t>& packet) {
+    LOG_INFO("🧠 Received Decision Packet | Size: {} bytes", packet.size());
+
+    // TODO: Parse Node Identity Vector (NIV)
+    // TODO: Parse Path Intelligence Vector (PIV)
+    // TODO: Parse Intent Descriptor (IDR)
+
+    // FIXME: Integrate with FSM dispatch
+}
+
+}  // namespace control_plane
+}  // namespace atrop
