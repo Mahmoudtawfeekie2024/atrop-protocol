a850b5acd2307956194bb7c5ebee9e23ad73a170
Mahmoudtawfeekie2024 <168466008+Mahmoudtawfeekie2024@users.noreply.github.com>
2025-07-04 10:21:55 +0100

    Update decision_handler.hpp

--- daemon/handlers/decision_handler.hpp
+++ daemon/handlers/decision_handler.hpp
@@ -1,6 +1,4 @@
-// daemon/ipc/decision_handler.hpp
-
 #pragma once
 #include <string>
 
-void handleDecisionPacket(const std::string& raw_packet);
+std::string handleDecisionPacket(const std::string& raw_packet);
